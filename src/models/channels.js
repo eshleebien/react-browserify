f661b3a317b24aba251d46a2e77697d88714f56f
Eshlee Romero <esh@any.tv>
2015-06-18 02:11:34 +0800

    added test data

--- src/models/channels.js
+++ src/models/channels.js
@@ -5,7 +5,8 @@ var Request = require('superagent');
 module.exports = function (data, next) {
     var start = function () {
         Request
-            .get('http://l1.freedom.tm:3000/channels')
+            //.get('http://l1.freedom.tm:3000/channels?per_page=20')
+            .get('channels.json')
             .end(next);
     }
 
